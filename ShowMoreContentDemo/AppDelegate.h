d299546072ca3f696a76eba385486897a08a03b5
ZJQian <2326274522@qq.com>
2017-02-09 10:49:51 +0800

    Initial Commit

---
+++ ShowMoreContentDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ShowMoreContentDemo
+//
+//  Created by ZJQ on 2017/2/9.
+//  Copyright © 2017年 ZJQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ShowMoreContentDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ShowMoreContentDemo
+//
+//  Created by ZJQ on 2017/2/9.
+//  Copyright © 2017年 ZJQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
